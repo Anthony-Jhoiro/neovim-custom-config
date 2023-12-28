f2c4a60a257a61c3cf7e6aaa2ddf2a6fe92a6be7
anthony <quereantho@gmail.com>
2023-12-28 17:02:08 +0100

    :sparkles: Add WTF Dashboard on boot

--- init.lua
+++ init.lua
@@ -3,6 +3,17 @@ vim.g.copilot_assume_mapped = true
 
 vim.api.nvim_create_user_command("Git", ":terminal lazygit && exit", {})
 
+-- Dashboard
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Do not open dashboard on startup when editing files
+    if next(vim.fn.argv()) == nil then
+      vim.cmd ":terminal wtf"
+      vim.cmd "NvimTreeToggle"
+    end
+  end,
+})
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
